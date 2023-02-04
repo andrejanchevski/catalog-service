0c79e0e738b2ddae2777d5ff1ea3799daf0587a9
anchevski <andrej.anchevski@rtl-extern.de>
2023-02-04 16:39:03 +0100

    Failing tests because of exception message fixed at Book Validation Tests

--- src/main/java/com/polarobookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarobookshop/catalogservice/domain/Book.java
@@ -14,17 +14,17 @@ public record Book(
         @Id
         Long id,
 
-        @NotBlank(message = "The book ISBN must be defined")
+        @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(
                 regexp = "^([0-9]{10}|[0-9]{13}$)",
-                message = "The ISBN format must be valid"
+                message = "The ISBN format must be valid."
         )
         String isbn,
         @NotBlank(
-                message = "The book title must be defined"
+                message = "The book title must be defined."
         )
         String title,
-        @NotBlank(message = "The book author must be defined")
+        @NotBlank(message = "The book author must be defined.")
         String author,
 
         @NotNull(message = "The book price must be defined.")
