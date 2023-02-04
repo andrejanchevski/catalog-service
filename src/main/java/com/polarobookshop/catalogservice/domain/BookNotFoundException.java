d0a66c102cd4f0c308f40740300c5697178d7031
anchevski <andrej.anchevski@rtl-extern.de>
2023-02-04 16:34:53 +0100

    Failing tests because of exception message fixed.

--- src/main/java/com/polarobookshop/catalogservice/domain/BookNotFoundException.java
+++ src/main/java/com/polarobookshop/catalogservice/domain/BookNotFoundException.java
@@ -2,6 +2,6 @@ package com.polarobookshop.catalogservice.domain;
 
 public class BookNotFoundException extends RuntimeException{
     public BookNotFoundException(String isbn){
-        super("The book with ISBN " + isbn + " was not found");
+        super("The book with ISBN " + isbn + " was not found.");
     }
 }
